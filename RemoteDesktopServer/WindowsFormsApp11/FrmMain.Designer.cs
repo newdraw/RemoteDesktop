23e8bfac94e1f15fc79da3035b8b51614df4178a
newdraw <newdraw@hotmail.com>
2023-08-01 14:30:34 +0800

    server code

---
+++ RemoteDesktopServer/WindowsFormsApp11/FrmMain.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.trackDitherLevels = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboDitherType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackKeepLevels = new System.Windows.Forms.TrackBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picHistogram = new System.Windows.Forms.PictureBox();
+            this.chkShowInfo = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackDitherLevels)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackKeepLevels)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picHistogram)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(102, 47);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(330, 21);
+            this.txtIP.TabIndex = 0;
+            this.txtIP.Text = "192.168.123.172";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(456, 45);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始投屏";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // trackDitherLevels
+            // 
+            this.trackDitherLevels.Location = new System.Drawing.Point(137, 137);
+            this.trackDitherLevels.Maximum = 64;
+            this.trackDitherLevels.Minimum = 1;
+            this.trackDitherLevels.Name = "trackDitherLevels";
+            this.trackDitherLevels.Size = new System.Drawing.Size(394, 45);
+            this.trackDitherLevels.TabIndex = 3;
+            this.trackDitherLevels.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackDitherLevels.Value = 7;
+            this.trackDitherLevels.Scroll += new System.EventHandler(this.trackDitherLevels_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "仿色等级";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "仿色方法";
+            // 
+            // cboDitherType
+            // 
+            this.cboDitherType.DisplayMember = "图案";
+            this.cboDitherType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDitherType.FormattingEnabled = true;
+            this.cboDitherType.Items.AddRange(new object[] {
+            "图案",
+            "杂色",
+            "无"});
+            this.cboDitherType.Location = new System.Drawing.Point(142, 101);
+            this.cboDitherType.Name = "cboDitherType";
+            this.cboDitherType.Size = new System.Drawing.Size(204, 20);
+            this.cboDitherType.TabIndex = 7;
+            this.cboDitherType.ValueMember = "图案";
+            this.cboDitherType.SelectedIndexChanged += new System.EventHandler(this.cboDitherType_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "保留区间";
+            // 
+            // trackKeepLevels
+            // 
+            this.trackKeepLevels.Location = new System.Drawing.Point(137, 173);
+            this.trackKeepLevels.Maximum = 100;
+            this.trackKeepLevels.Minimum = 10;
+            this.trackKeepLevels.Name = "trackKeepLevels";
+            this.trackKeepLevels.Size = new System.Drawing.Size(394, 45);
+            this.trackKeepLevels.TabIndex = 8;
+            this.trackKeepLevels.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackKeepLevels.Value = 90;
+            this.trackKeepLevels.Scroll += new System.EventHandler(this.trackKeepLevels_Scroll);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.picHistogram);
+            this.groupBox1.Location = new System.Drawing.Point(71, 243);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 237);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "直方图";
+            // 
+            // picHistogram
+            // 
+            this.picHistogram.Location = new System.Drawing.Point(7, 21);
+            this.picHistogram.Name = "picHistogram";
+            this.picHistogram.Size = new System.Drawing.Size(354, 210);
+            this.picHistogram.TabIndex = 0;
+            this.picHistogram.TabStop = false;
+            // 
+            // chkShowInfo
+            // 
+            this.chkShowInfo.AutoSize = true;
+            this.chkShowInfo.Location = new System.Drawing.Point(71, 210);
+            this.chkShowInfo.Name = "chkShowInfo";
+            this.chkShowInfo.Size = new System.Drawing.Size(108, 16);
+            this.chkShowInfo.TabIndex = 11;
+            this.chkShowInfo.Text = "显示帧数和流量";
+            this.chkShowInfo.UseVisualStyleBackColor = true;
+            this.chkShowInfo.CheckedChanged += new System.EventHandler(this.chkShowInfo_CheckedChanged);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 506);
+            this.Controls.Add(this.chkShowInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackKeepLevels);
+            this.Controls.Add(this.cboDitherType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackDitherLevels);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackDitherLevels)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackKeepLevels)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picHistogram)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TrackBar trackDitherLevels;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboDitherType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackKeepLevels;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox picHistogram;
+        private System.Windows.Forms.CheckBox chkShowInfo;
+    }
+}
+
---
+++ RemoteDesktopServer/WindowsFormsApp11/FrmMain.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+using System.Drawing.Imaging; 
+using System.Net.Sockets;
+using System.Threading;
+using System.Drawing.Drawing2D;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp11
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+         
+          
+        Random rnd = new Random();
+        const int frameWidth = 128;
+        const int frameHeight = 64;
+        const int totalPixels = frameWidth * frameHeight;
+
+        double keepLevels = 0.9;
+        int ditherLevels = 7;
+        DitherType ditherType = DitherType.Pattern;
+
+        enum DitherType
+        {
+            Pattern = 0,
+            Random = 1,
+            None = 2
+        }
+
+        /// <summary>
+        /// 截取屏幕
+        /// </summary>
+        unsafe byte[] snapshot(out int[] histogram)
+        {
+            var width = Screen.PrimaryScreen.Bounds.Width;
+            var height = Screen.PrimaryScreen.Bounds.Height;
+            using var img = new Bitmap(width, height);
+            using var g = Graphics.FromImage(img);
+
+            histogram = new int[256];
+
+            var pixels = new byte[totalPixels];
+
+            g.CopyFromScreen(0, 0, 0, 0, img.Size, CopyPixelOperation.SourceCopy);
+
+            var bits = img.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            var p = (byte*)bits.Scan0;
+
+            for (var y = 0; y < frameHeight; y++)
+                for (var x = 0; x < frameWidth; x++)
+                {
+                    var srcX = x * (width / frameWidth);
+                    var srcY = y * (height / frameHeight);
+                    var pb = p + ((srcY * width) + srcX) * 3;
+                    var pg = pb + 1;
+                    var pr = pb + 2;
+                    var pixel = (byte)(*pr * 0.299 + *pg * 0.587 + *pb * 0.114);
+
+                    pixels[y * frameWidth + x] = pixel;
+                    histogram[pixel]++;
+                }
+
+            img.UnlockBits(bits);
+
+            return pixels;
+        }
+
+        /// <summary>
+        /// 裁剪输出区间
+        /// </summary>
+        /// <param name="histogram"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        void cutLevels(int[] histogram, out byte min, out byte max)
+        {
+            min = byte.MinValue;
+            max = byte.MaxValue; 
+
+            double totalLevels = frameWidth * frameHeight;
+            double outLevels = totalLevels;
+
+            while (true)
+            {
+                if (max - min < ditherLevels * 2) //保留给仿色等级，防止黑白不分
+                {
+                    break;
+                }
+
+                //牺牲量小的一端
+                if (histogram[min] > histogram[max])
+                {
+                    if ((outLevels - histogram[max]) / totalLevels > keepLevels)
+                    {
+                        max--;
+                        outLevels -= histogram[max];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if ((outLevels - histogram[min]) / totalLevels > keepLevels)
+                    {
+                        min++;
+                        outLevels -= histogram[min];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            } 
+        }
+
+        /// <summary>
+        /// 计算帧数据
+        /// </summary>
+        /// <param name="ditherType"></param>
+        /// <param name="pixels"></param>
+        /// <param name="histogram"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        byte[] makeFrame(DitherType ditherType, byte[] pixels, int[] histogram, byte min, byte max)
+        {
+
+            var middleLevel = 0;
+            if (ditherType == DitherType.None)
+            {
+                var sum = 0;
+                for (int i = min; i <= max; i++)
+                {
+                    sum += histogram[i];
+                }
+                var count = 0;
+                for (var i = min; i <= max; i++)
+                {
+                    count += histogram[i];
+                    if (count >= sum / 2)
+                    {
+                        middleLevel = i;
+                        break;
+                    }
+                }
+            }
+
+            var data = new byte[frameWidth * frameHeight / 8];
+            for (var pos = 0; pos < pixels.Length; pos++)
+            {
+
+                var x = pos % frameWidth;
+                var y = pos / frameWidth;
+                var draw = false;
+                if (ditherType == DitherType.None)
+                {
+                    draw = pixels[pos] > middleLevel;
+                }
+                else
+                {
+                    var level = (pixels[pos] - min) / Math.Max(1, (max - min) / ditherLevels);
+                    ////(x+y*height)*level
+                    /////(x + y * (height * 1.1)) * level
+                    /////((pos % width)+(pos/width)*height
+                    var value = ditherType == DitherType.Random ? rnd.Next() :
+                        (x + y * (frameHeight + level )* 1.5) ;
+                    draw = value % ditherLevels < level;
+                }
+
+                if (draw)
+                {
+                    var i = pos / 8;
+                    var bit = pos % 8;
+                    data[i] |= (byte)(1 << bit);
+                }
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// 计算帧差
+        /// </summary>
+        /// <param name="currFrame"></param>
+        /// <param name="lastFrame"></param>
+        /// <returns></returns>
+        byte[] findFrameDiff(byte[] currFrame, byte[] lastFrame)
+        {
+            var frameDiff = new List<byte>();
+            if (lastFrame != null)
+            {
+                short fromIndex = -1;
+                var bytes = new List<byte>();
+
+                for (short i = 0; i < currFrame.Length; i++)
+                {
+                    var last = lastFrame[i];
+                    var curr = currFrame[i];
+                    if (last != curr)
+                    {
+                        if (fromIndex == -1)
+                        {
+                            fromIndex = i;
+                        }
+                        bytes.Add(curr);
+                    }
+                    else
+                    {
+                        if (fromIndex != -1)
+                        {
+                            frameDiff.AddRange(BitConverter.GetBytes(fromIndex));
+                            frameDiff.AddRange(BitConverter.GetBytes((short)bytes.Count));
+                            frameDiff.AddRange(bytes);
+
+                            fromIndex = -1;
+                            bytes.Clear();
+                        }
+                    }
+                }
+            }
+            return frameDiff.ToArray();
+        }
+
+        /// <summary>
+        /// 发送数据
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="frameData"></param>
+        /// <param name="frameDiff"></param>
+        /// <param name="isFirstFrame"></param>
+        void sendData(NetworkStream stream, byte[] frameData, byte[] frameDiff, bool isFirstFrame)
+        {
+
+            const byte DATA_FRAME = 0;
+            const byte DATA_FRAMEDIFF = 1;
+            lock (stream)
+            {
+                if (isFirstFrame || frameDiff.Length > frameData.Length)
+                {
+                    stream.WriteByte(DATA_FRAME);
+                    stream.Write(frameData, 0, frameData.Length);
+                }
+                else
+                {
+                    var dat = frameDiff.ToArray();
+                    stream.WriteByte(DATA_FRAMEDIFF);
+                    stream.Write(BitConverter.GetBytes((short)dat.Length), 0, 2);
+                    stream.Write(dat, 0, dat.Length);
+                }
+                stream.Flush();
+            }
+        }
+
+        /// <summary>
+        /// 画直方图
+        /// </summary>
+        /// <param name="histogram"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        Bitmap paintHistogram(int[] histogram, byte min, byte max)
+        {
+            using var penLimit = new Pen(Color.Red);
+            penLimit.DashStyle = DashStyle.Dot; 
+            var imgHistogram = new Bitmap(byte.MaxValue + 1, 200);
+            using var gHistogram = Graphics.FromImage(imgHistogram);
+            gHistogram.Clear(Color.White);
+            var histMax = histogram.Max();
+
+            for (var i = 0; i <= byte.MaxValue; i++)
+            {
+                if (histogram[i] == 0)
+                {
+                    continue;
+                }
+                if (histogram[i] == histMax)
+                {
+
+                }
+                gHistogram.DrawLine(
+                     Pens.Black,
+                    i, imgHistogram.Height, i, imgHistogram.Height - (histogram[i] * imgHistogram.Height / histMax));
+            }
+            gHistogram.DrawLine(penLimit, min, 0, min, imgHistogram.Height);
+            gHistogram.DrawLine(penLimit, max, 0, max, imgHistogram.Height);
+            return imgHistogram;
+        }
+
+        NetworkStream stream = null;
+
+        unsafe void workProc()
+        {
+
+            long frames = 0;
+
+            var lastMin = byte.MinValue;
+            var lastMax = byte.MaxValue;
+            byte[] lastFrame = null;
+
+            using var tcp = new TcpClient();
+            tcp.Connect(txtIP.Text, 5720);
+            stream = tcp.GetStream();
+            while (true)
+            {
+                var pixels = snapshot(out var histogram);
+                cutLevels(histogram, out var min, out var max);
+
+                //从上一个区间平滑过渡，防止闪烁
+                min = (byte)(min + (lastMin - min) / 3);
+                max = (byte)(max + (lastMax - max) / 3);
+
+                lastMin = min;
+                lastMax = max;
+
+                var frame = makeFrame(ditherType, pixels, histogram, min, max);
+                var frameDiff = findFrameDiff(frame, lastFrame);
+                sendData(stream, frame, frameDiff, frames == 0);
+
+                if (frames % 2 == 0)
+                {
+                    using var imgHistogram = paintHistogram(histogram, min, max);
+                    using var g = picHistogram.CreateGraphics();
+                    g.DrawImage(imgHistogram, 0, 0);
+                }
+
+                lastFrame = frame;
+                frames++;
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtIP.Enabled = false;
+            btnStart.Enabled = false;
+            new Thread(workProc) { IsBackground = true }.Start();
+        }
+
+        private void trackDitherLevels_Scroll(object sender, EventArgs e)
+        {
+            ditherLevels = trackDitherLevels.Value;
+        }
+
+        private void trackKeepLevels_Scroll(object sender, EventArgs e)
+        {
+            keepLevels = trackKeepLevels.Value / 100d;
+        }
+
+        private void cboDitherType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ditherType= (DitherType) cboDitherType.SelectedIndex; 
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            cboDitherType.SelectedIndex = 0;
+        }
+
+        private void chkShowInfo_CheckedChanged(object sender, EventArgs e)
+        {
+            if(stream!=null)
+            {
+                lock(stream)
+                {
+                    const byte DATA_CONFIG = 2;
+
+                    stream.WriteByte(DATA_CONFIG);
+                    stream.WriteByte((byte)(chkShowInfo.Checked?1:0));
+                    stream.Flush();
+                }
+            }
+        }
+    }
+}
